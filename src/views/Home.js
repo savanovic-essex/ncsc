73d64646b4e9c8822e8075d5953552b7ab638987
Djordje Savanovic <ds21546@essex.ac.uk>
2022-07-20 20:35:08 +0200

    Cleaned style for homepage

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,23 +1,62 @@
-import logo from '../logo.svg';
 import '../App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Card, CardBody} from "reactstrap";
+import {
+    Button,
+    Card,
+    CardBody, CardText, CardTitle, Col, Container,
+    Navbar,
+    NavbarBrand, Row,
+} from "reactstrap";
 
 function Home() {
+
     return (
-        <div className="App">
-            <div className="App-header">
-                <Card style={{marginRight: 10}}>
-                    <CardBody>
-                        <p>kdlsfjkdjflkdsjfdsfjldsf</p>
-                    </CardBody>
-                </Card>
-                <Card style={{marginLeft: 10}}>
-                    <CardBody>
-                        <p>kdlsfjkdjflkdsjfdsfjldsf</p>
-                    </CardBody>
-                </Card>
-            </div>
+        <div>
+            <Navbar
+                color="secondary"
+                dark
+                fixed={"top"}
+            >
+                <NavbarBrand href="/">
+                    NCSC
+                </NavbarBrand>
+            </Navbar>
+            <Container fluid className="container-bg">
+                <Row style={{paddingTop: '15%'}}>
+                    <Col md={{size: 4, offset: 2}}>
+                        <Card
+                            body
+                            className="text-center my-2"
+                        >
+                            <CardTitle tag="h5">
+                                Submit a report
+                            </CardTitle>
+                            <CardText>
+                                Readily available to public
+                            </CardText>
+                            <Button color="primary">
+                                Submit a report
+                            </Button>
+                        </Card>
+                    </Col>
+                    <Col md={{size: 4}}>
+                        <Card
+                            body
+                            className="text-center my-2"
+                        >
+                            <CardTitle tag="h5">
+                                Log in
+                            </CardTitle>
+                            <CardText>
+                                Log in as a cyber specialist
+                            </CardText>
+                            <Button color="primary">
+                                Log in
+                            </Button>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
