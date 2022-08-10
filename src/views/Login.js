aab0bf383dafc37843e9961d4189b6c4ea34ef56
Djordje Savanovic <ds21546@essex.ac.uk>
2022-08-10 14:52:57 +0200

    Implemented 2fa

--- src/views/Login.js
+++ src/views/Login.js
@@ -7,7 +7,12 @@ import {useState} from "react";
 import {useNavigate} from "react-router-dom";
 import {
     signInWithEmailAndPassword,
+    getMultiFactorResolver,
+    PhoneAuthProvider,
+    PhoneMultiFactorGenerator,
+    RecaptchaVerifier
 } from "firebase/auth";
+import 'firebase/auth';
 import {auth} from "../firebase.js";
 import CustomNavbar from "../components/Navbar";
 import {Helmet} from "react-helmet";
@@ -19,14 +24,46 @@ function Login() {
 
     const navigate = useNavigate();
 
-    const logIn = () => {
+    let resolver;
+    let multiFactorHints;
+
+    const logIn = async () => {
+        auth.tenantId = "ncsc-zzcf6";
         signInWithEmailAndPassword(auth, email, password)
             .then((res) => {
                 navigate("/dashboard");
                 console.log(JSON.stringify(res, null, 2));
             })
-            .catch((err) => {
-                console.log(JSON.stringify(err, null, 2));
+            .catch(async (error) => {
+                if (error.code === 'auth/multi-factor-auth-required') {
+                    resolver = getMultiFactorResolver(auth, error);
+                    // Show UI to let user select second factor.
+                    multiFactorHints = resolver.hints;
+                    const selectedHint = multiFactorHints[0];
+
+                    const recaptchaVerifier = new RecaptchaVerifier('recaptcha-container', {
+                        'size': 'invisible',
+                    }, auth);
+                    const phoneAuthProvider = new PhoneAuthProvider(auth);
+                    const phoneInfoOptions = {
+                        multiFactorHint: selectedHint,
+                        session: resolver.session
+                    };
+                    // Send SMS verification code
+                    const verificationId = await phoneAuthProvider.verifyPhoneNumber(phoneInfoOptions, recaptchaVerifier)
+
+                    let verificationCode = window.prompt('Please enter the verification ' +
+                        'code that was sent to your mobile device.');
+                    const cred = PhoneAuthProvider.credential(
+                        verificationId, verificationCode);
+                    const multiFactorAssertion =
+                        PhoneMultiFactorGenerator.assertion(cred);
+                    // Complete sign-in.
+                    const userCredential = await resolver.resolveSignIn(multiFactorAssertion);
+                    navigate("/dashboard")
+                } else {
+                    console.log(error)
+                }
             });
     };
 
@@ -72,10 +109,11 @@ function Login() {
                                     />
                                 </div>
                                 <div className="d-grid">
-                                    <button className="btn btn-primary" onClick={logIn}>
+                                    <button className="btn btn-primary my-2" onClick={logIn}>
                                         Submit
                                     </button>
                                 </div>
+                                <div id={'recaptcha-container'}/>
                             </CardBody>
                         </Card>
                     </Col>
