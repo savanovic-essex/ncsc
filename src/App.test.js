5e904ddca3ba80e171264b45993237c21fc3468e
Mohammad Atieh <mohammad.attieh@gmail.com>
2022-08-14 14:15:30 +0300

    commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,19 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+<<<<<<< Updated upstream
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
+=======
+it('renders CustomNavbar', () => {
+  render(
+      <BrowserRouter>
+        <Routes>
+          <Route path="*" element= {<CustomNavbar />}/>
+        </Routes>
+      </BrowserRouter>);
+  const linkElement = screen.queryByTestId('custom-navbar');
+>>>>>>> Stashed changes
   expect(linkElement).toBeInTheDocument();
 });
