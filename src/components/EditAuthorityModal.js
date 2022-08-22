d4db2760f9d6c6205197e6b0009ec68dac7d69b9
Djordje Savanovic <ds21546@essex.ac.uk>
2022-08-22 23:54:44 +0200

    Style change

--- src/components/EditAuthorityModal.js
+++ src/components/EditAuthorityModal.js
@@ -39,7 +39,7 @@ const EditAuthorityModal = ({uidd, toggle, modal}) => {
 
     return (
         <Modal isOpen={modal} toggle={toggle} data-testid="edit-authority-modal">
-            <ModalHeader toggle={toggle}>Modal title</ModalHeader>
+            <ModalHeader toggle={toggle}>Edit Authority</ModalHeader>
             <ModalBody>
                 <Row>
                     <Col md={6}>
